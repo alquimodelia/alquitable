48b6f548e43d7065c410ad634cc2879cf7b5fd1d
JotaFan <jotaflame@gmail.com>
2023-10-30 14:40:17 +0000

    Add advanced losses

--- alquitable/__init__.py
+++ alquitable/__init__.py
@@ -1,3 +1,3 @@
-from alquitable import callbacks, losses
+from alquitable import advanced_losses, callbacks, losses
 
-__all__ = ["losses", "callbacks"]
+__all__ = ["losses", "callbacks", "advanced_losses"]
