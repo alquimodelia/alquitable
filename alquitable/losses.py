9554a9b3e027d1c9b72fb101e16a9ef6fa27db21
JotaFan <jotaflame@gmail.com>
2023-10-30 14:40:42 +0000

    Refactor losses to become subclasses of keras_core.Loss

--- alquitable/losses.py
+++ alquitable/losses.py
@@ -1,46 +1,40 @@
 import inspect
 import sys
 
-import keras_core
-from keras_core import ops
+from keras_core import Loss, ops
 
-OFFSET = 30
 
-@keras_core.saving.register_keras_serializable()
-def weighted_loss(y_true, y_pred):
-    # Calculate weights based on the absolute difference
-    #   between true and predicted values
-    weights = ops.abs(y_true - y_pred) 
-    loss = ops.square(y_true - y_pred)  # Calculate the squared loss
-    # Multiply the weights with the loss
-    weighted_loss = ops.multiply(weights, loss)  
-    return ops.mean(weighted_loss)  # Calculate the mean of the weighted loss
+class MeanSquaredDiffError(Loss):
+    def call(self, y_true, y_pred):
+        return ops.mean(ops.abs(ops.square(y_pred) - ops.square(y_true)))
 
-@keras_core.saving.register_keras_serializable()
-def mean_squared_diff_error(y_true, y_pred):
-    return ops.mean(ops.abs(ops.square(y_pred) - ops.square(y_true)))
 
-@keras_core.saving.register_keras_serializable()
-def mean_squared_error(y_true, y_pred):
-    erro = y_pred - y_true
-    se = ops.square(erro)
-    mse = ops.mean(se)
-    return mse
-
-@keras_core.saving.register_keras_serializable()
-def mean_squared_diff_log_error(y_true, y_pred):
-    return ops.mean(
-        ops.abs(
-            ops.log(ops.square(y_true) + 1) - ops.log(ops.square(y_pred) + 1)
+class MeanSquaredDiffLogError(Loss):
+    def call(self, y_true, y_pred):
+        return ops.mean(
+            ops.abs(
+                ops.log(ops.square(y_true) + 1)
+                - ops.log(ops.square(y_pred) + 1)
+            )
         )
-    )
 
 
-@keras_core.saving.register_keras_serializable()
-def mean_absolute_percentage_error(y_true, y_pred):
-    return (ops.abs(y_pred - y_true) + OFFSET) / (y_true + OFFSET)
+class MeanCubicError(Loss):
+    def call(self, y_true, y_pred):
+        erro = y_pred - y_true
+        ce = ops.multiply(ops.square(erro), ops.abs(erro))
+        mce = ops.mean(ce)
+        return mce
 
 
 module = inspect.currentframe().f_globals["__name__"]
-# Get all the functions defined in the module
-loss_functions = inspect.getmembers(sys.modules[module], inspect.isfunction)
+
+
+# Define a predicate function to filter out Loss subclasses
+def is_loss_subclass(cls):
+    return inspect.isclass(cls) and issubclass(cls, Loss)
+
+
+# Get all the Loss subclasses defined in the module
+loss_functions = inspect.getmembers(sys.modules[module], is_loss_subclass)
+loss_functions = [(n, f) for n, f in loss_functions if n != "Loss"]
